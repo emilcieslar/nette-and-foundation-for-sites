3e485140f637e8b723c38f4981c870f3018740ad
Emil Cieslar <emil.cieslar@gmail.com>
2016-04-22 23:42:06 +0200

    Debug mode switched

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -5,7 +5,6 @@ require __DIR__ . '/../vendor/autoload.php';
 $configurator = new Nette\Configurator;
 
 //$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
-$configurator->setDebugMode(FALSE);
 $configurator->enableDebugger(__DIR__ . '/../log');
 
 $configurator->setTempDirectory(__DIR__ . '/../temp');
